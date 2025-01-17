a82ec90e9b6f6e946bc9922e5f2c2059538dfae0
Ntrie1 <114010893+Ntrie1@users.noreply.github.com>
2025-01-17 21:25:56 +0200

    values from input stored in appjsx

--- todolist-reactjs/src/App.jsx
+++ todolist-reactjs/src/App.jsx
@@ -3,18 +3,30 @@ import TodoInput from "./components/TodoInput"
 import TodoList from "./components/TodoList"
 
 function App() {
-const [todos, setTodos] = useState([])
+  const [todos, setTodos] = useState([]);
+  const [todoValue, setTodoValue] = useState([]);
 
-function handleAddTodos(newTodo){
-  const newTodoList = [...todos, newTodo]
-  setTodos(newTodoList)
-  
-}
+  function handleAddTodos(newTodo) {
+    const newTodoList = [...todos, newTodo]
+    setTodos(newTodoList)
+
+  }
+
+  function handleDeleteTodo(index) {
+    const newTodoList = todos.filter((todo, todoIndex) => {
+      return todoIndex !== index
+    })
+    setTodos(newTodoList)
+  }
+
+  function handleEditTodo(index) {
+
+  }
 
   return (
     <>
-      <TodoInput handleAddTodos={handleAddTodos} />
-      <TodoList todos={todos} />
+      <TodoInput todoValue={todoValue} setTodosValue={setTodoValue} handleAddTodos={handleAddTodos} />
+      <TodoList handleDeleteTodo={handleDeleteTodo} todos={todos} />
     </>
   )
 }
