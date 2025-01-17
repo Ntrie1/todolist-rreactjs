d35d49d34f6252ed0fda3f36b67366d6518cad91
Ntrie1 <114010893+Ntrie1@users.noreply.github.com>
2025-01-17 22:19:47 +0200

    made data to persist

--- todolist-reactjs/src/App.jsx
+++ todolist-reactjs/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import TodoInput from "./components/TodoInput"
 import TodoList from "./components/TodoList"
 
@@ -6,9 +6,15 @@ function App() {
   const [todos, setTodos] = useState([]);
   const [todoValue, setTodoValue] = useState([]);
 
+
+  function persistData(newList) {
+    localStorage.setItem('todos', JSON.stringify({ todos: newList }))
+  }
+
   function handleAddTodos(newTodo) {
-    const newTodoList = [...todos, newTodo]
-    setTodos(newTodoList)
+    const newTodoList = [...todos, newTodo];
+    setTodos(newTodoList);
+    persistData(newTodoList);
 
   }
 
@@ -16,20 +22,35 @@ function App() {
     const newTodoList = todos.filter((todo, todoIndex) => {
       return todoIndex !== index
     })
-    setTodos(newTodoList)
+    setTodos(newTodoList);
+    persistData(newTodoList);
   }
 
   function handleEditTodo(index) {
     const valueToBeEdited = todos[index];
     setTodoValue(valueToBeEdited);
     handleDeleteTodo(index)
-   
+
   }
+
+  useEffect(() => {
+    if (!localStorage) {
+      return;
+    }
+
+    let localTodos = localStorage.getItem('todos');
+
+    if (!localTodos) {
+      return;
+    }
+    localTodos = JSON.parse(localTodos).todos
+    setTodos(localTodos);
+  }, [])
 
   return (
     <>
       <TodoInput todoValue={todoValue} setTodoValue={setTodoValue} handleAddTodos={handleAddTodos} />
-      <TodoList handleDeleteTodo={handleDeleteTodo} handleEditTodo={handleEditTodo}  todos={todos} />
+      <TodoList handleDeleteTodo={handleDeleteTodo} handleEditTodo={handleEditTodo} todos={todos} />
     </>
   )
 }
--- todolist-reactjs/src/components/TodoList.jsx
+++ todolist-reactjs/src/components/TodoList.jsx
@@ -3,7 +3,8 @@ import TodoCard from './TodoCard'
 
 export default function TodoList(props) {
     const {todos} = props;
-
+    console.log(todos);
+    
     return (
         <ul className='main'>
             {todos.map((todo, todoIndex) => {
